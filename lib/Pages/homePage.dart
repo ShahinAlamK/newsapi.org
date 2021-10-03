5e279783f720b5362006474ec301a4f6af169056
ShahinAlamK <91818093+ShahinAlamK@users.noreply.github.com>
2021-10-03 20:31:31 +0600

    news

--- lib/Pages/homePage.dart
+++ lib/Pages/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newsapi/Services/ApiService.dart';
 
 class HomePage extends StatefulWidget {
 
@@ -7,9 +8,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    NewsApiService().fetchingApi();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Text("NewsApp"),
+      ),
+
 
     );
   }
---
+++ lib/Services/ApiService.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart'as http;
+import 'package:newsapi/Models/newsModel.dart';
+
+class NewsApiService{
+
+  Future<Article?>fetchingApi()async{
+    String url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3120fffd5de5418796fbab343da2dacc";
+    try{
+      final response=await http.get(Uri.parse(url));
+      if(response.statusCode==200){
+        final decode=await jsonDecode(response.body);
+        if(decode['status']=="ok"){
+          print(decode);
+        }
+      }
+    }catch(e){
+      print(e.toString());
+    }
+  }
+
+}
